0ffe4af8addc5919a2371b15a97196a77bdd8145
unique <yunik.maharjan@icloud.com>
2021-06-19 22:13:30 +0545

    keep alive issue fix

---
+++ tunnel.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/url"
+	"os"
+	"os/user"
+	"time"
+)
+
+func getParams() string {
+	params := url.Values{}
+	params.Add("username",
+		func() string {
+			if *subdomain == "" {
+				username, _ := user.Current()
+				return username.Username
+			}
+			return *subdomain
+		}())
+
+	return params.Encode()
+}
+func openTunnel() {
+
+	u := url.URL{Scheme: "wss", Host: *host, Path: "/_ws/", RawQuery: getParams()}
+	fmt.Printf("\u001B[34mConnecting to %s \n\n", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	keepAlive(c, time.Minute)
+
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	message, err := ReadHandshakeMessage(c)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	//fmt.Printf("\u001B[31m Your are now online at: https://%s \n\n", message.Host)
+	fmt.Printf("\033[32m%-25s Online\033[00m \n", "Tunnel Status")
+	fmt.Printf("%-25s https://%s -> http://127.0.0.1:%s \n\n", "Forwarded", message.Host, *port)
+	client := Client{
+		host:  fmt.Sprintf("http://127.0.0.1:%s", *port),
+		token: message.Token,
+		conn:  c,
+	}
+	for {
+		message, err := ReadMessage(c)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go client.process(message)
+	}
+}
